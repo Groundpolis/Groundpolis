b0711df24921326a81226ea56d2bf4b3f8de201b
mei23 <m@m544.net>
2018-09-19 04:14:34 +0900

    Add max-age to AP user / publickey

--- src/server/activitypub.ts
+++ src/server/activitypub.ts
@@ -122,6 +122,7 @@ router.get('/users/:user/publickey', async ctx => {
 
 	if (isLocalUser(user)) {
 		ctx.body = pack(renderKey(user));
+		ctx.set('Cache-control', 'max-age=60, public');
 		setResponseType(ctx);
 	} else {
 		ctx.status = 400;
@@ -136,6 +137,7 @@ async function userInfo(ctx: Router.IRouterContext, user: IUser) {
 	}
 
 	ctx.body = pack(await renderPerson(user as ILocalUser));
+	ctx.set('Cache-control', 'max-age=60, public');
 	setResponseType(ctx);
 }
 
