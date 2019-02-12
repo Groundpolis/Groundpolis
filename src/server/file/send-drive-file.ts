c277df270c80bf09731ff80396f73924e330ead5
mei23 <m@m544.net>
2019-02-13 02:59:51 +0900

    410

--- src/server/file/send-drive-file.ts
+++ src/server/file/send-drive-file.ts
@@ -36,12 +36,6 @@ export default async function(ctx: Koa.BaseContext) {
 		return;
 	}
 
-	if (file.metadata.deletedAt) {
-		ctx.status = 410;
-		await send(ctx as any, '/tombstone.png', { root: assets });
-		return;
-	}
-
 	if (file.metadata.withoutChunks && (file.metadata.isRemote || file.metadata._user && file.metadata._user.host != null)) {
 		// urlは過去のバグで張り替え忘れている可能性があるためuriを優先する
 		const url = file.metadata.uri || file.metadata.url;
@@ -92,6 +86,12 @@ export default async function(ctx: Koa.BaseContext) {
 		} finally {
 			cleanup();
 		}
+		return;
+	}
+
+	if (file.metadata.deletedAt) {
+		ctx.status = 410;
+		await send(ctx as any, '/tombstone.png', { root: assets });
 		return;
 	}
 
