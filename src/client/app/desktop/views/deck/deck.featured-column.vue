544c641c734c08f1ed5ab93ad3d0c58617939652
mei23 <m@m544.net>
2019-02-18 01:24:32 +0900

    featured 7 days

--- src/client/app/desktop/views/deck/deck.featured-column.vue
+++ src/client/app/desktop/views/deck/deck.featured-column.vue
@@ -43,7 +43,7 @@ export default Vue.extend({
 			(this.$refs.timeline as any).init(() => new Promise((res, rej) => {
 				this.$root.api('notes/featured', {
 					limit: 20,
-					days: 1000,
+					days: 7,
 				}).then(notes => {
 					notes.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 					res(notes);
--- src/client/app/mobile/views/pages/featured.vue
+++ src/client/app/mobile/views/pages/featured.vue
@@ -37,7 +37,7 @@ export default Vue.extend({
 
 			this.$root.api('notes/featured', {
 				limit: 20,
-				days: 1000,
+				days: 7,
 			}).then(notes => {
 				notes.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 				this.notes = notes;
