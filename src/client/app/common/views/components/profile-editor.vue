c402f73b44679ac32df8b5a3ef68e474bd6a8890
mei23 <m@m544.net>
2019-02-21 15:22:00 +0900

    Disable deleteAccount

--- src/client/app/common/views/components/profile-editor.vue
+++ src/client/app/common/views/components/profile-editor.vue
@@ -105,7 +105,6 @@
 	<section>
 		<details>
 			<summary>{{ $t('danger-zone') }}</summary>
-			<ui-button @click="deleteAccount()">{{ $t('delete-account') }}</ui-button>
 		</details>
 	</section>
 </ui-card>
