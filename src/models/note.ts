02356ea9841b6de6465ca117da277f9499d2e779
mei23 <m@m544.net>
2019-03-03 18:15:55 +0900

    Revert "カスタム絵文字リアクション"
    This reverts commit 4544752a64252fee684ca045eaf92b9bc204245c.

--- src/models/note.ts
+++ src/models/note.ts
@@ -11,7 +11,6 @@ import Reaction from './note-reaction';
 import { packMany as packFileMany, IDriveFile } from './drive-file';
 import Following from './following';
 import Emoji from './emoji';
-import { concat, unique } from '../prelude/array';
 
 const Note = db.get<INote>('notes');
 Note.createIndex('uri', { sparse: true, unique: true });
@@ -248,10 +247,6 @@ export const pack = async (
 				fields: { _id: false }
 			});
 		} else {
-			if (_note.reactionCounts) {
-				_note.emojis = unique(concat([_note.emojis, Object.keys(_note.reactionCounts).map(x => x.replace(/:/g, ''))]));
-			}
-
 			_note.emojis = Emoji.find({
 				name: { $in: _note.emojis },
 				host: host
