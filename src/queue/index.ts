e8c33f41bfed3e0dae69b6d37852d541cde94102
mei23 <m@m544.net>
2019-03-08 07:07:27 +0900

    clean

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -33,6 +33,8 @@ setInterval(() => {
 	inboxQueue.getJobCounts().then(c => {
 		console.log(`inboxQueue: wait: ${c.waiting}, active: ${c.active}, completed: ${c.completed}, failed: ${c.failed}, delayed ${c.delayed}`);
 	});
+
+	deliverQueue.clean(60 * 1000, 'active');
 }, 10 * 1000);
 
 export function deliver(user: ILocalUser, content: any, to: any) {
@@ -132,7 +134,6 @@ export default function() {
 	if (!program.onlyServer) {
 		deliverQueue.process(128, processDeliver);
 		inboxQueue.process(128, processInbox);
-		deliverQueue.clean(60 * 1000, 'active');
 		processDb(dbQueue);
 	}
 }
