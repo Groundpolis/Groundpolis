100ed4695d8713853c1fe08f4736a789de08dbdc
mei23 <m@m544.net>
2019-03-23 20:38:54 +0900

    絵文字のaltをresolvableに

--- src/client/app/common/views/components/emoji.vue
+++ src/client/app/common/views/components/emoji.vue
@@ -1,5 +1,5 @@
 <template>
-<img v-if="customEmoji" class="fvgwvorwhxigeolkkrcderjzcawqrscl custom" :class="{ normal: normal }" :src="url" :alt="alt" :title="alt"/>
+<img v-if="customEmoji" class="fvgwvorwhxigeolkkrcderjzcawqrscl custom" :class="{ normal: normal }" :src="url" :alt="alt" :title="title"/>
 <img v-else-if="char && !useOsDefaultEmojis" class="fvgwvorwhxigeolkkrcderjzcawqrscl" :src="url" :alt="alt" :title="alt"/>
 <span v-else-if="char && useOsDefaultEmojis">{{ char }}</span>
 <span v-else>:{{ name }}:</span>
@@ -47,6 +47,10 @@ export default Vue.extend({
 
 	computed: {
 		alt(): string {
+			return this.customEmoji ? `:${this.customEmoji.resolvable}:` : this.char;
+		},
+
+		title(): string {
 			return this.customEmoji ? `:${this.customEmoji.name}:` : this.char;
 		},
 
