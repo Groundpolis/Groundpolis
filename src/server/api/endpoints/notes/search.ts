8b37770fe36c6f61e3cba0be2f107de4c84e1022
mei23 <m@m544.net>
2019-03-26 01:32:03 +0900

    search from

--- src/server/api/endpoints/notes/search.ts
+++ src/server/api/endpoints/notes/search.ts
@@ -5,6 +5,10 @@ import { packMany } from '../../../../models/note';
 import es from '../../../../db/elasticsearch';
 import define from '../../define';
 import { ApiError } from '../../error';
+import User, { IUser, ILocalUser } from '../../../../models/user';
+import { toDbHost } from '../../../../misc/convert-host';
+import Following from '../../../../models/following';
+const escapeRegexp = require('escape-regexp');
 
 export const meta = {
 	desc: {
@@ -49,6 +53,9 @@ export const meta = {
 };
 
 export default define(meta, async (ps, me) => {
+	const internal = await searchInternal(me, ps.query, ps.limit, ps.offset).catch(e => console.log(e));
+	if (internal !== null) return internal;
+
 	if (es == null) throw new ApiError(meta.errors.searchingNotAvailable);
 
 	const response = await es.search({
@@ -89,3 +96,71 @@ export default define(meta, async (ps, me) => {
 
 	return await packMany(notes, me);
 });
+
+async function searchInternal(me: ILocalUser, query: string, limit: number, offset: number) {
+	// extract tokens
+	const tokens = query.trim().split(/\s+/);
+	const words: string[] = [];
+	let from: IUser = null;
+
+	for (const token of tokens) {
+		// from
+		const matchFrom = token.match(/^from:@?([\w-]+)(?:@([\w.-]+))?$/);
+		if (matchFrom) {
+			const user = await User.findOne({
+				usernameLower: matchFrom[1].toLowerCase(),
+				host: toDbHost(matchFrom[2]),
+			});
+
+			if (user == null) return [];	// fromが存在しないユーザーならno match
+			from = user;
+			continue;
+		}
+
+		words.push(token);
+	}
+
+	if (from == null) return null;	// fromが指定されてなかったら検索させない
+
+	// constract query
+	const isFollowing = me == null ? false : ((await Following.findOne({
+		followerId: me._id,
+		followeeId: from._id
+	})) != null);
+
+	const visibleQuery = me == null ? [{
+		visibility: { $in: ['public', 'home'] }
+	}] : [{
+		visibility: {
+			$in: isFollowing ? ['public', 'home', 'followers'] : ['public', 'home']
+		}
+	}, {
+		// myself (for specified/private)
+		userId: me._id
+	}, {
+		// to me (for specified)
+		visibleUserIds: { $in: [ me._id ] }
+	}];
+
+	const noteQuery = {
+		$and: [ {} ],
+		deletedAt: null,
+		userId: from._id,
+		$or: visibleQuery
+	} as any;
+
+	for (const word of words) {
+		noteQuery.$and.push({
+			text: new RegExp(escapeRegexp(word))
+		});
+	}
+
+	// console.log(JSON.stringify(noteQuery, null, 2));
+
+	const notes = await Note.find(noteQuery, {
+		limit,
+		skip: offset,
+	});
+
+	return await packMany(notes, me);
+}
