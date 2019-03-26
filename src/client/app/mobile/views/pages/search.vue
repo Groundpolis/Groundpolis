5da3fb5c40f1dc6cebd85d02d01c97963b94fac8
mei23 <m@m544.net>
2019-03-26 18:03:24 +0900

    dec limit

--- src/client/app/mobile/views/pages/search.vue
+++ src/client/app/mobile/views/pages/search.vue
@@ -13,7 +13,7 @@ import Vue from 'vue';
 import i18n from '../../../i18n';
 import Progress from '../../../common/scripts/loading';
 
-const limit = 20;
+const limit = 10;
 
 export default Vue.extend({
 	i18n: i18n('mobile/views/pages/search.vue'),
