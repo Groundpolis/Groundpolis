51c74a7e8597dbb99ea3086b1d74d2dfca66cae4
mei23 <m@m544.net>
2019-04-02 00:00:44 +0900

    like to 3rd

--- src/remote/activitypub/kernel/like.ts
+++ src/remote/activitypub/kernel/like.ts
@@ -1,21 +1,29 @@
-import * as mongo from 'mongodb';
-import Note from '../../../models/note';
 import { IRemoteUser } from '../../../models/user';
 import { ILike } from '../type';
 import create from '../../../services/note/reaction/create';
+import { resolveNote } from '../models/note';
+import { apLogger } from '../logger';
+
+const logger = apLogger;
 
 export default async (actor: IRemoteUser, activity: ILike) => {
 	const id = typeof activity.object == 'string' ? activity.object : activity.object.id;
 
-	// Transform:
-	// https://misskey.ex/notes/xxxx to
-	// xxxx
-	const noteId = new mongo.ObjectID(id.split('/').pop());
-
-	const note = await Note.findOne({ _id: noteId });
-	if (note === null) {
-		throw new Error();
+	// like対象をresolve
+	let note;
+	try {
+		note = await resolveNote(id);
+	} catch (e) {
+		// 対象が4xxならスキップ
+		if (e.statusCode >= 400 && e.statusCode < 500) {
+			logger.warn(`Ignored like target ${id} - ${e.statusCode}`);
+			return;
+		}
+		logger.warn(`Error in like target ${id} - ${e.statusCode || e}`);
+		throw e;
 	}
+
+	logger.info(`Like: ${actor.uri} => ${id}(${note._id})`);
 
 	await create(actor, note, activity._misskey_reaction);
 };
