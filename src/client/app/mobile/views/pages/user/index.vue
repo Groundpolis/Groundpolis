04af41d91936e81f9a99856f5019f89d4b878129
mei23 <m@m544.net>
2019-04-08 13:52:41 +0900

    Fix: ユーザー表示を切り替えてもフォローボタンの状態が変わらない

--- src/client/app/mobile/views/pages/user/index.vue
+++ src/client/app/mobile/views/pages/user/index.vue
@@ -14,7 +14,7 @@
 						<img :src="avator" alt="avatar"/>
 					</a>
 					<button class="menu" ref="menu" @click="menu"><fa icon="ellipsis-h"/></button>
-					<mk-follow-button v-if="$store.getters.isSignedIn && $store.state.i.id != user.id" :user="user"/>
+					<mk-follow-button v-if="$store.getters.isSignedIn && $store.state.i.id != user.id" :user="user" :key="user.id"/>
 				</div>
 				<div class="title">
 					<h1><mk-user-name :user="user" :key="user.id"/></h1>
