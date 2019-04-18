00a0831ea0697e883c37b8f9491323a4d6400367
mei23 <m@m544.net>
2019-04-18 21:00:59 +0900

    ワードミュートがCWに効くように, 大文字小文字を区別しないように

--- src/client/app/common/scripts/should-mute-note.ts
+++ src/client/app/common/scripts/should-mute-note.ts
@@ -1,3 +1,5 @@
+import { INote } from '../../../../models/note';
+
 export default function(me, settings, note) {
 	const isMyNote = me && (note.userId == me.id);
 	const isPureRenote = note.renoteId != null && note.text == null && note.fileIds.length == 0 && note.poll == null;
@@ -5,15 +7,18 @@ export default function(me, settings, note) {
 	const includesMutedWords = (text: string) =>
 		text
 			? settings.mutedWords.some(q => q.length > 0 && !q.some(word =>
-				word.startsWith('/') && word.endsWith('/') ? !(new RegExp(word.substr(1, word.length - 2)).test(text)) : !text.includes(word)))
+				word.startsWith('/') && word.endsWith('/') ? !(new RegExp(word.substr(1, word.length - 2), 'i').test(text)) : !text.toLowerCase().includes(word.toLowerCase())))
 			: false;
 
+	const includesMutedWordsInNote = (note: INote) =>
+		(note.text && includesMutedWords(note.text)) || (note.cw && includesMutedWords(note.cw));
+
 	return (
-		(!isMyNote && note.reply && includesMutedWords(note.reply.text)) ||
-		(!isMyNote && note.renote && includesMutedWords(note.renote.text)) ||
+		(!isMyNote && note.reply && includesMutedWordsInNote(note.reply)) ||
+		(!isMyNote && note.renote && includesMutedWordsInNote(note.renote)) ||
 		(settings.showMyRenotes === false && isMyNote && isPureRenote) ||
 		(settings.showRenotedMyNotes === false && isPureRenote && note.renote.userId == me.id) ||
 		(settings.showLocalRenotes === false && isPureRenote && note.renote.user.host == null) ||
-		(!isMyNote && includesMutedWords(note.text))
+		(!isMyNote && includesMutedWordsInNote(note))
 	);
 }
