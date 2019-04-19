64122134d04b4d3ba61d773de1ad56b7e323dc82
mei23 <m@m544.net>
2019-04-20 06:17:20 +0900

    ミューブロリストをリンク

--- src/client/app/common/views/components/settings/mute-and-block.vue
+++ src/client/app/common/views/components/settings/mute-and-block.vue
@@ -7,7 +7,9 @@
 		<ui-info v-if="!muteFetching && mute.length == 0">{{ $t('no-muted-users') }}</ui-info>
 		<div class="users" v-if="mute.length != 0">
 			<div v-for="user in mute" :key="user.id">
-				<p><b><mk-user-name :user="user"/></b> @{{ user | acct }}</p>
+				<router-link class="name" :to="user | userPage" v-user-preview="user.id">
+					<b><mk-user-name :user="user"/></b> @{{ user | acct }}
+				</router-link>
 			</div>
 		</div>
 	</section>
@@ -17,7 +19,9 @@
 		<ui-info v-if="!blockFetching && block.length == 0">{{ $t('no-blocked-users') }}</ui-info>
 		<div class="users" v-if="block.length != 0">
 			<div v-for="user in block" :key="user.id">
-				<p><b><mk-user-name :user="user"/></b> @{{ user | acct }}</p>
+				<router-link class="name" :to="user | userPage" v-user-preview="user.id">
+					<b><mk-user-name :user="user"/></b> @{{ user | acct }}
+				</router-link>
 			</div>
 		</div>
 	</section>
