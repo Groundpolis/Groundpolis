04f8f523e52387373df6a38e72091cf58821e8b8
mei23 <m@m544.net>
2019-04-25 05:11:05 +0900

    Follow twemoji 12.0.1

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,3 +1,4 @@
-export const twemojiBase = 'https://cdn.jsdelivr.net/npm/twemoji@12.0.0';
-// https://cdn.jsdelivr.net/npm/twemoji@12.0.0
+export const twemojiBase = 'https://cdn.jsdelivr.net/npm/twemoji@12.0.1';
+// https://cdn.jsdelivr.net/npm/twemoji@12.0.1
+// https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.1
 // https://twemoji.maxcdn.com
