5a58c99ad5bbb97c949956309a81df8f9e16f50f
mei23 <m@m544.net>
2019-05-10 19:19:35 +0900

    ダッシュボードを軽く

--- src/client/app/admin/views/dashboard.vue
+++ src/client/app/admin/views/dashboard.vue
@@ -8,12 +8,6 @@
 		<p>{{ $t('@.ai-chan-kawaii') }}</p>
 	</header>
 
-	<marquee-text v-if="instances.length > 0" class="instances" :repeat="10" :duration="60">
-		<span v-for="instance in instances" class="instance">
-			<b :style="{ background: instance.bg }">{{ instance.host }}</b>{{ instance.notesCount | number }} / {{ instance.usersCount | number }}
-		</span>
-	</marquee-text>
-
 	<div v-if="stats" class="stats">
 		<div>
 			<div>
@@ -80,10 +74,6 @@
 	<div class="cpu-memory">
 		<x-cpu-memory :connection="connection"/>
 	</div>
-
-	<div class="ap">
-		<x-ap-log/>
-	</div>
 </div>
 </template>
 
@@ -93,10 +83,7 @@ import i18n from '../../i18n';
 import XCpuMemory from "./dashboard.cpu-memory.vue";
 import XQueue from "./dashboard.queue-charts.vue";
 import XCharts from "./dashboard.charts.vue";
-import XApLog from "./dashboard.ap-log.vue";
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
-import MarqueeText from 'vue-marquee-text-component';
-import randomColor from 'randomcolor';
 
 export default Vue.extend({
 	i18n: i18n('admin/views/dashboard.vue'),
@@ -105,8 +92,6 @@ export default Vue.extend({
 		XCpuMemory,
 		XQueue,
 		XCharts,
-		XApLog,
-		MarqueeText
 	},
 
 	data() {
@@ -114,7 +99,6 @@ export default Vue.extend({
 			stats: null,
 			connection: null,
 			meta: null,
-			instances: [],
 			clock: null,
 			faDatabase
 		};
@@ -124,22 +108,10 @@ export default Vue.extend({
 		this.connection = this.$root.stream.useSharedConnection('serverStats');
 
 		this.updateStats();
-		this.clock = setInterval(this.updateStats, 1000);
+		this.clock = setInterval(this.updateStats, 60 * 1000);
 
 		this.$root.getMeta().then(meta => {
 			this.meta = meta;
-		});
-
-		this.$root.api('federation/instances', {
-			sort: '+notes'
-		}).then(instances => {
-			for (const i of instances) {
-				i.bg = randomColor({
-					seed: i.host,
-					luminosity: 'dark'
-				});
-			}
-			this.instances = instances;
 		});
 	},
 
@@ -194,20 +166,6 @@ export default Vue.extend({
 			&:last-child
 				margin-left auto
 				margin-right 0
-
-	> .instances
-		padding 16px
-		color var(--adminDashboardHeaderFg)
-		font-size 13px
-
-		>>> .instance
-			margin 0 10px
-
-			> b
-				padding 2px 6px
-				margin-right 4px
-				border-radius 4px
-				color #fff
 
 	> .stats
 		display flex
