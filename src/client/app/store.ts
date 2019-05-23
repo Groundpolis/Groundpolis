3013aac4e824b5f0811fa2c09a8845859fbc8797
mei23 <m@m544.net>
2019-05-23 15:58:11 +0900

    デフォルトテーマが変わっていたのを修正

--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -51,7 +51,7 @@ const defaultDeviceSettings = {
 	roundedCorners: true,
 	reduceMotion: false,
 	darkmode: true,
-	darkTheme: 'bb5a8287-a072-4b0a-8ae5-ea2a0d33f4f2',
+	darkTheme: 'promo',
 	lightTheme: 'light',
 	lineWidth: 1,
 	fontSize: 0,
