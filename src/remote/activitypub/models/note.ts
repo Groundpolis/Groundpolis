dbd48aea7e5b445ea764fa88f39b0451ba8fcc32
mei23 <m@m544.net>
2019-06-01 21:29:16 +0900

    リモートカスタム絵文字Updateトリガを変更

--- src/remote/activitypub/models/note.ts
+++ src/remote/activitypub/models/note.ts
@@ -295,6 +295,7 @@ export async function extractEmojis(tags: ITag[], host_: string) {
 				if ((tag.updated != null && exists.updatedAt == null)
 					|| (tag.id != null && exists.uri == null)
 					|| (exists.url != tag.icon.url)
+					|| (exists.updatedAt != null && Date.now() - exists.updatedAt.getTime() > 7 * 86400 * 1000)
 					|| (tag.updated != null && exists.updatedAt != null && new Date(tag.updated) > exists.updatedAt)) {
 						logger.info(`update emoji host=${host}, name=${name}`);
 						return await Emoji.findOneAndUpdate({
