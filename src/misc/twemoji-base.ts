18dfa04a18ba8aefa85fabddae6343274c0b3f94
mei23 <m@m544.net>
2019-06-22 13:54:03 +0900

    Change Twemoji CDN to CloudFlare

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,4 +1,4 @@
-export const twemojiBase = 'https://cdn.jsdelivr.net/npm/twemoji@12.0.1';
+export const twemojiBase = 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.1';
 // https://cdn.jsdelivr.net/npm/twemoji@12.0.1
 // https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.1
 // https://twemoji.maxcdn.com
