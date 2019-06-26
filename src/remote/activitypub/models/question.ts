739d31a1f53e6e1bf5a97c5265a605e112c621bd
mei23 <m@m544.net>
2019-06-27 08:39:56 +0900

    Fix Note / Question type

--- src/remote/activitypub/models/question.ts
+++ src/remote/activitypub/models/question.ts
@@ -1,11 +1,18 @@
 import config from '../../../config';
 import Note, { IChoice, IPoll } from '../../../models/note';
 import Resolver from '../resolver';
-import { IQuestion } from '../type';
+import { IObject, IQuestion, isQuestion,  } from '../type';
 import { apLogger } from '../logger';
 
-export async function extractPollFromQuestion(source: string | IQuestion): Promise<IPoll> {
-	const question = typeof source === 'string' ? await new Resolver().resolve(source) as IQuestion : source;
+export async function extractPollFromQuestion(source: string | IObject, resolver?: Resolver): Promise<IPoll> {
+	if (resolver == null) resolver = new Resolver();
+
+	const question = await resolver.resolve(source);
+
+	if (!isQuestion(question)) {
+		throw new Error('invalid type');
+	}
+
 	const multiple = !question.oneOf;
 	const expiresAt = question.endTime ? new Date(question.endTime) : null;
 
