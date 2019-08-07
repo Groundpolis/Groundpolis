735409205fce85730623305b1d016cffc592de0b
mei23 <m@m544.net>
2019-08-07 10:03:48 +0900

    static URLを短くする

--- src/client/app/common/scripts/get-static-image-url.ts
+++ src/client/app/common/scripts/get-static-image-url.ts
@@ -3,7 +3,7 @@ import * as url from '../../../../prelude/url';
 
 export function getStaticImageUrl(baseUrl: string): string {
 	const u = new URL(baseUrl);
-	const dummy = `${u.host}${u.pathname}`;	// 拡張子がないとキャッシュしてくれないCDNがあるので
+	const dummy = `${u.pathname.replace(/.*\//, '')}`;	// 拡張子がないとキャッシュしてくれないCDNがあるので
 	return `${instanceUrl}/proxy/${dummy}?${url.query({
 		url: u.href,
 		static: '1'
