2136f4ab7070482c146e7618b3fd9b1322728148
mei23 <m@m544.net>
2019-09-15 02:56:59 +0900

    ハッシュタグ内Keycap Number Signはなおすのやめる

--- test/mfm.ts
+++ test/mfm.ts
@@ -674,13 +674,6 @@ describe('MFM', () => {
 					leaf('emoji', { emoji: '#️⃣' })
 				]);
 			});
-
-			it('allow Keycap Number Sign (U+0023 + U+FE0F + U+20E3)', () => {
-				const tokens = parse('##️⃣');
-				assert.deepStrictEqual(tokens, [
-					leaf('hashtag', { hashtag: '#️⃣' })
-				]);
-			});
 		});
 
 		describe('quote', () => {
