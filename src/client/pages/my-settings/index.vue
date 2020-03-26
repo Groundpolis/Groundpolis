3458a6b585f3bb63a0b58b7a6a9cc4cfe8d99ecf
Xeltica <xeltica@gmail.com>
2020-03-26 21:59:37 +0900

    設定にプロフィール編集ボタン追加

--- src/client/pages/my-settings/index.vue
+++ src/client/pages/my-settings/index.vue
@@ -3,6 +3,10 @@
 	<portal to="icon"><fa :icon="faCog"/></portal>
 	<portal to="title">{{ $t('settings') }}</portal>
 
+	<router-link to="/my/settings/profile" class="_panel _buttonPrimary">
+		{{ $t('editProfile') }}
+	</router-link>
+
 	<x-privacy-setting/>
 	<x-reaction-setting/>
 
