cc8b426dbb2c13a79c0afcfe548488b0792e303c
Xeltica <xeltica@gmail.com>
2020-04-25 14:22:02 +0900

    resolve #32

--- src/client/router.ts
+++ src/client/router.ts
@@ -50,6 +50,7 @@ export const router = new VueRouter({
 		{ path: '/my/apps', component: page('apps') },
 		{ path: '/preferences', component: page('preferences/index') },
 		{ path: '/scratchpad', component: page('scratchpad') },
+		{ path: '/games', component: page('under-construction') },
 		{ path: '/instance', component: page('instance/index') },
 		{ path: '/instance/emojis', component: page('instance/emojis') },
 		{ path: '/instance/users', component: page('instance/users') },
