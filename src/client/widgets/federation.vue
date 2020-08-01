1e546f9f2f0874ecc476af5840f93640d41747de
Xeltica <xeltica@gmail.com>
2020-08-01 18:50:27 +0900

    resolve #113

--- src/client/widgets/federation.vue
+++ src/client/widgets/federation.vue
@@ -8,7 +8,7 @@
 			<div v-for="(instance, i) in instances" :key="instance.id" class="instance">
 				<img v-if="instance.iconUrl" :src="instance.iconUrl" alt=""/>
 				<div class="body">
-					<a class="a" :href="'https://' + instance.host" target="_blank" :title="instance.host">{{ instance.host }}</a>
+					<a class="a" :href="'https://' + instance.host" target="_blank" :title="instance.host" v-text="instance.name || instance.host" />
 					<p>{{ instance.softwareName || '?' }} {{ instance.softwareVersion }}</p>
 				</div>
 				<mk-mini-chart class="chart" :src="charts[i].requests.received"/>
