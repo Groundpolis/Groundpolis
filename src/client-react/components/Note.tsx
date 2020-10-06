a296170ef0f92160dd4b19d3b6de6acac4d32cf2
Xeltica <xeltica@gmail.com>
2020-10-06 23:07:52 +0900

    renote表示

--- src/client-react/components/Note.tsx
+++ src/client-react/components/Note.tsx
@@ -15,56 +15,73 @@ import { PackedUser } from '../../models/repositories/user';
 
 import './Note.scss';
 
-export default function Note(props: { note: PackedNote }) {
-	const [ cwOpen, setCwOpen ] = useState(false);
-	const user = props.note.user as PackedUser;
-	const isMine = (reaction: string) => props.note.myReaction === reaction;
+function RenotedBy({ user }: { user: PackedUser }) { 
+	return (
+		<div className="renoted-by">
+			<img className="avatar" src={user.avatarUrl} alt={getAcct(user)} />
+			<FontAwesomeIcon icon={faRetweet} />
+			{ t('renotedBy', { user: user.name || user.username }) }
+		</div>
+	);
+}
+
+export default function Note(props: { note: PackedNote, pinned: boolean }) {
+	const isPureRenote = props.note.renote && !props.note.text;
+
+	const note = isPureRenote ? props.note.renote as PackedNote : props.note;
+	const renoter = isPureRenote ? (props.note.user as PackedUser) : null;
+	const [cwOpen, setCwOpen] = useState(false);
+	const user = note.user as PackedUser;
+	const isMine = (reaction: string) => note.myReaction === reaction;
 	return (
 		<article className="_com note">
-			<img src={user.avatarUrl} className="avatar" alt={getAcct(user)} />
-			<section className="right">
-				<header>
-					<span className="name">
-						{ user.name ?? user.username }
-					</span>
-					<span className="acct">
-						@{ getAcct(user) }
-					</span>
-				</header>
-				<div className="content">
-					{props.note.cw ? (
-						<>
-							{ props.note.cw }
-							<button className="cw-button" onClick={() => setCwOpen(!cwOpen)}>{ cwOpen ? t('_cw.hide') : t('_cw.show') }</button>
-						</>
-					) : null }
-					{ !props.note.cw || cwOpen ? props.note.text : null }
-				</div>
-				<div className="reactions">
-					{
-						Object.entries(props.note.reactions as Record<string, number>)
-							.map(([ reaction, count ]) => (
-								<button key={reaction} className={ `_button reaction ${isMine(reaction) ? 'primary' : ''}`}>
-									{reaction} {count}
-								</button>
-							))
-					}
-				</div>
-				<div className="commands">
-					<button className="_button command">
-						<FontAwesomeIcon icon={faReply}/>
-					</button>
-					<button className="_button command">
-						<FontAwesomeIcon icon={faRetweet}/>
-					</button>
-					<button className="_button command" style={{ color: props.note.myReaction ? 'var(--accent' : undefined }}>
-						<FontAwesomeIcon icon={props.note.myReaction ? faMinus : faPlus}/>
-					</button>
-					<button className="_button command">
-						<FontAwesomeIcon icon={faEllipsisV}/>
-					</button>
-				</div>
-			</section>
+			{ isPureRenote ? <RenotedBy user={renoter} /> : null }
+			<div className="body">
+				<img src={user.avatarUrl} className="avatar" alt={getAcct(user)} />
+				<section className="right">
+					<header>
+						<span className="name">
+							{user.name ?? user.username}
+						</span>
+						<span className="acct">
+							@{getAcct(user)}
+						</span>
+					</header>
+					<div className="content">
+						{note.cw ? (
+							<>
+								{note.cw}
+								<button className="cw-button" onClick={() => setCwOpen(!cwOpen)}>{cwOpen ? t('_cw.hide') : t('_cw.show')}</button>
+							</>
+						) : null}
+						{!note.cw || cwOpen ? note.text : null}
+					</div>
+					<div className="reactions">
+						{
+							Object.entries(note.reactions as Record<string, number>)
+								.map(([reaction, count]) => (
+									<button key={reaction} className={`_button reaction ${isMine(reaction) ? 'primary' : ''}`}>
+										{reaction} {count}
+									</button>
+								))
+						}
+					</div>
+					<div className="commands">
+						<button className="_button command">
+							<FontAwesomeIcon icon={faReply} />
+						</button>
+						<button className="_button command">
+							<FontAwesomeIcon icon={faRetweet} />
+						</button>
+						<button className="_button command" style={{ color: note.myReaction ? 'var(--accent' : undefined }}>
+							<FontAwesomeIcon icon={note.myReaction ? faMinus : faPlus} />
+						</button>
+						<button className="_button command">
+							<FontAwesomeIcon icon={faEllipsisV} />
+						</button>
+					</div>
+				</section>
+			</div>
 		</article>
 	);
 }
--- src/client-react/utils/i18n.ts
+++ src/client-react/utils/i18n.ts
@@ -2,6 +2,7 @@ import VueI18n from 'vue-i18n';
 import { deepEntries, delimitEntry } from 'deep-entries';
 
 import { clientDb, clear, bulkSet } from '../db';
+import escapeRegExp from 'escape-regexp';
 
 let i18nMessages: Record<string, string | undefined> = { };
 let locale = '';
@@ -18,12 +19,18 @@ export function setI18nContexts(lang: string, version: string, cleardb = false) 
 		locale = lang;
 		i18nMessages = Object.fromEntries(flatLocaleEntries);
 	});
+	
 }
 
 export function setLocale(mes: Record<string, string | undefined>) {
 	i18nMessages = mes;
 }
 
-export function t(key: string): string {
-	return i18nMessages[key] || key;
+export function t(key: string, interpolation?: Record<string, unknown>): string {
+	let t = (i18nMessages[key] || key);
+	for (const key in interpolation) {
+		const value = interpolation[key];
+		t = t.replace(new RegExp(escapeRegExp(`{${key}}`), 'g'), () => String(value));
+	}
+	return t;
 }
