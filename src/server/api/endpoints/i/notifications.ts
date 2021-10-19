6dfda01c3489df0c6b18f82f5ce6682761489802
syuilo <Syuilotan@yahoo.co.jp>
2021-10-19 20:25:47 +0900

    fix(api): アプリケーション通知が取得できない問題を修正
    Fix #6702

--- src/server/api/endpoints/i/notifications.ts
+++ src/server/api/endpoints/i/notifications.ts
@@ -5,6 +5,8 @@ import define from '../../define';
 import { makePaginationQuery } from '../../common/make-pagination-query';
 import { Notifications, Followings, Mutings, Users } from '../../../../models';
 import { notificationTypes } from '../../../../types';
+import read from '../../../../services/note/read';
+import { Brackets } from 'typeorm';
 
 export const meta = {
 	desc: {
@@ -88,10 +90,16 @@ export default define(meta, async (ps, user) => {
 		.andWhere(`notification.notifieeId = :meId`, { meId: user.id })
 		.leftJoinAndSelect('notification.notifier', 'notifier');
 
-	query.andWhere(`notification.notifierId NOT IN (${ mutingQuery.getQuery() })`);
+	query.andWhere(new Brackets(qb => { qb
+		.where(`notification.notifierId NOT IN (${ mutingQuery.getQuery() })`)
+		.orWhere('notification.notifierId IS NULL');
+	}));
 	query.setParameters(mutingQuery.getParameters());
 
-	query.andWhere(`notification.notifierId NOT IN (${ suspendedQuery.getQuery() })`);
+	query.andWhere(new Brackets(qb => { qb
+		.where(`notification.notifierId NOT IN (${ suspendedQuery.getQuery() })`)
+		.orWhere('notification.notifierId IS NULL');
+	}));
 
 	if (ps.following) {
 		query.andWhere(`((notification.notifierId IN (${ followingQuery.getQuery() })) OR (notification.notifierId = :meId))`, { meId: user.id });
