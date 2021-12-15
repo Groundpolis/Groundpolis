759bd1e21d18d7202c6c93be8a9fb05157142bd8
Xeltica <xeltica@gmail.com>
2021-12-15 09:23:50 +0900

    resolve #366 and resolve #378

--- src/client/pages/welcome.timeline.vue
+++ src/client/pages/welcome.timeline.vue
@@ -1,10 +1,22 @@
 <template>
 <div class="civpbkhh">
-	<div v-for="note in notes" class="note">
-		<div class="content _panel">
-			<Mfm plain no-sticker :text="note.text" />
+	<div ref="scroll" class="scrollbox" v-bind:class="{ scroll: isScrolling }">
+		<div v-for="note in notes" :key="note.id" class="note">
+			<div class="content _panel">
+				<div class="body">
+					<MkA v-if="note.replyId" class="reply" :to="`/notes/${note.replyId}`"><i class="fas fa-reply"></i></MkA>
+					<Mfm v-if="note.text" :text="note.text" :author="note.user" :i="$i" :custom-emojis="note.emojis"/>
+					<MkA v-if="note.renoteId" class="rp" :to="`/notes/${note.renoteId}`">RN: ...</MkA>
+				</div>
+				<div v-if="note.files.length > 0" class="richcontent">
+					<XMediaList :media-list="note.files"/>
+				</div>
+				<div v-if="note.poll">
+					<XPoll :note="note" :readOnly="true"/>
+				</div>
+			</div>
+			<XReactionsViewer ref="reactionsViewer" :note="note"/>
 		</div>
-		<XReactionsViewer :note="note" ref="reactionsViewer"/>
 	</div>
 </div>
 </template>
@@ -12,19 +24,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import XReactionsViewer from '@/components/reactions-viewer.vue';
+import XMediaList from '@/components/media-list.vue';
+import XPoll from '@/components/poll.vue';
 import * as os from '@/os';
-
 export default defineComponent({
 	components: {
-		XReactionsViewer
+		XReactionsViewer,
+		XMediaList,
+		XPoll
 	},
-
 	data() {
 		return {
 			notes: [],
+			isScrolling: false,
 		}
 	},
-
 	created() {
 		const ep = 
 			!this.$instance.disableFeatured 
@@ -34,23 +48,47 @@ export default defineComponent({
 		os.api(ep).then(notes => {
 			this.notes = notes.filter(n => n.text);
 		});
+	},
+	updated() {
+		if (this.$refs.scroll.clientHeight > window.innerHeight) {
+			this.isScrolling = true;
+		}
 	}
 });
 </script>
 
 <style lang="scss" scoped>
+@keyframes scroll {
+	0% {
+		transform: translate3d(0, 0, 0);
+	}
+	5% {
+		transform: translate3d(0, 0, 0);
+	}
+	75% {
+		transform: translate3d(0, calc(-100% + 90vh), 0);
+	}
+	90% {
+		transform: translate3d(0, calc(-100% + 90vh), 0);
+	}
+}
 .civpbkhh {
 	text-align: right;
-
-	> .note {
-		margin: 16px 0 16px auto;
-
-		> .content {
-			padding: 16px;
-			margin: 0 0 0 auto;
-			max-width: max-content;
-			border-radius: 16px;
-			max-height: 10rem;
+	> .scrollbox {
+		&.scroll {
+			animation: scroll 45s linear infinite;
+		}
+		> .note {
+			margin: 16px 0 16px auto;
+			> .content {
+				padding: 16px;
+				margin: 0 0 0 auto;
+				max-width: max-content;
+				border-radius: 16px;
+				> .richcontent {
+					min-width: 250px;
+				}
+			}
 		}
 	}
 }
